beaedfbc1b78ab0457cc6bfb2118de67aca1eb36
Stefan Eckert <MrTelanie@gmail.com>
2018-08-12 14:23:22 +0200

    feat: vector normalize function tests

--- test/vector.js
+++ test/vector.js
@@ -34,4 +34,40 @@ describe('Vector test.', () => {
     assert.isFalse(dir.len < pos.len, `${dir} should be longer than ${pos}`);
 
   });
+
+  it('should change length to 1 when calling normalize', () => {
+
+    const pos = new Vector(5, 6, 7);
+    const dir = pos.normalize();
+
+    const length = dir.length;
+    assert(length > 0.99 && length < 1.01, `${dir} should have length 1, but is ${length}`);
+
+  });
+
+  it('only for readme', () => {
+
+    // create vector by numbers
+
+    const pos = new Vector(5, 6, 7);
+    const dir = new Vector(1, 0, 0);
+
+    console.log('pos:', pos, ' dir:', dir);
+    // pos: { [Number: 10.48] x: 5, y: 6, z: 7 }  dir: { [Number: 1] x: 1, y: 0, z: 0 }
+
+    // or create vector by calculating other vectors and number
+    const offset = new Vector(() => dir * 30 + pos);
+
+    console.log('offset:', offset);
+    // offset: { [Number: 36.19] x: 35, y: 6, z: 7 }
+
+    // compare length
+    let way = offset;
+    if (way > 1) {
+      way = way.normalize();
+    }
+    console.log('way:', way);
+    // way: { [Number: 1] x: 0.96, y: 0.16, z: 0.19 }
+
+  });
 });
