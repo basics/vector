7c29af4d665ff85de4253c33a4b86146c4b03eec
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-08-12 18:32:21 +0200

    fix(test): readme test imports example.js

--- test/index.js
+++ test/index.js
@@ -3,16 +3,13 @@ import Vector from '../src';
 
 describe('Vector test.', () => {
   it('should create x y z values', () => {
-
     const pos = new Vector(5, 6, 7);
     assert.equal(pos.x, 5);
     assert.equal(pos.y, 6);
     assert.equal(pos.z, 7);
-
   });
 
   it('should be calculated by assigned statement', () => {
-
     const pos = new Vector(5, 6, 7);
     const dir = new Vector(1, 0, 0);
     const scale = new Vector(() => dir * pos);
@@ -20,11 +17,9 @@ describe('Vector test.', () => {
     assert.equal(scale.x, 5);
     assert.equal(scale.y, 0);
     assert.equal(scale.z, 0);
-
   });
 
   it('should compares lengths of vector.', () => {
-
     const pos = new Vector(1, 1, 1);
     const dir = new Vector(2, 2, 2);
 
@@ -32,21 +27,17 @@ describe('Vector test.', () => {
     assert.isTrue(dir.len > pos.len, `${dir} should be longer than ${pos}`);
     assert.isFalse(dir < pos, `${dir} should be longer than ${pos}`);
     assert.isFalse(dir.len < pos.len, `${dir} should be longer than ${pos}`);
-
   });
 
   it('should change length to 1 when calling normalize', () => {
-
     const pos = new Vector(5, 6, 7);
     const dir = pos.normalize();
 
     const length = dir.length;
     assert(length > 0.99 && length < 1.01, `${dir} should have length 1, but is ${length}`);
-
   });
 
   it('should set opposite axis to 1 when calling the cross product', () => {
-
     const dir1 = new Vector(0, 1, 0);
     const dir2 = new Vector(-1, 0, 0);
     const cross = dir1.crossNormalize(dir2);
@@ -54,32 +45,9 @@ describe('Vector test.', () => {
     assert.equal(cross.x, 0);
     assert.equal(cross.y, 0);
     assert.equal(cross.z, 1);
-
   });
 
   it('only for readme', () => {
-
-    // create vector by numbers
-
-    const pos = new Vector(5, 6, 7);
-    const dir = new Vector(1, 0, 0);
-
-    console.log('pos:', pos, ' dir:', dir);
-    // pos: { [Number: 10.48] x: 5, y: 6, z: 7 }  dir: { [Number: 1] x: 1, y: 0, z: 0 }
-
-    // or create vector by calculating other vectors and number
-    const offset = new Vector(() => dir * 30 + pos);
-
-    console.log('offset:', offset);
-    // offset: { [Number: 36.19] x: 35, y: 6, z: 7 }
-
-    // compare length
-    let way = offset;
-    if (way > 1) {
-      way = way.normalize();
-    }
-    console.log('way:', way);
-    // way: { [Number: 1] x: 0.96, y: 0.16, z: 0.19 }
-
+    require('../examples');
   });
 });
