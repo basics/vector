0eb7be79509a9c6aa42b135143219ac73b5e016d
Stefan Eckert <MrTelanie@gmail.com>
2019-09-13 18:49:38 +0200

    fix(index): remove unneeded imports

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import { operatorCalc } from './operator';
+import { Vector } from './vector';
 
-import {
-  Vector, Victor, Victor as IVector, vector, victor, victor as ivector
-} from './vector';
-import {
-  Point, IPoint, point, ipoint
-} from './point';
 
 export {
-  Vector, Victor, Victor as IVector, vector, victor, victor as ivector
+  Vector, Victor, Victor as IVector, vector, victor, victor as ivector, FORWARD, LEFT, UP
 } from './vector';
 export {
   Point, IPoint, point, ipoint
 } from './point';
+
+
+/**
+ * @typedef {Vector & number} VectorType
+ * @typedef {import('./vector').Victor & number} VictorType
+ * @typedef {import('./point').Point & number} PointType
+ * @typedef {import('./point').IPoint & number} IPointType
+ */
 
 /**
  * @param {() => number} alg
- * @return {(Vector | Victor | IVector | Point | IPoint) & number | number}
+ * @return {VectorType | VictorType | PointType | IPointType | number}
  */
 export function calc(alg) {
   return operatorCalc(alg);
 }
 
 export default Vector;
-
-export const Export = {
-  Vector, Victor, IVector, Point, IPoint, vector, victor, ivector, point, ipoint
-};
