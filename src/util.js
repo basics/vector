34dc1e92ecdf3714747a4e6ea0902a3fa6058062
Stefan Eckert <MrTelanie@gmail.com>
2019-09-26 21:45:53 +0200

    feat(util): more features

--- src/util.js
+++ src/util.js
@@ -1,3 +1,35 @@
+
 export function isArray(arr) {
   return Array.isArray(arr) || ArrayBuffer.isView(arr);
+}
+
+export function multQuatVec(quat, vec) {
+  const {
+    x, y, z
+  } = vec;
+  const {
+    x: qx, y: qy, z: qz, w: qw
+  } = quat;
+
+  const ix = qw * x + qy * z - qz * y;
+  const iy = qw * y + qz * x - qx * z;
+  const iz = qw * z + qx * y - qy * x;
+  const iw = -qx * x - qy * y - qz * z;
+  const rx = ix * qw + iw * -qx + iy * -qz - iz * -qy;
+  const ry = iy * qw + iw * -qy + iz * -qx - ix * -qz;
+  const rz = iz * qw + iw * -qz + ix * -qy - iy * -qx;
+  return new vec.constructor(rx, ry, rz);
+}
+
+const angle180 = Math.PI;
+const angle360 = Math.PI * 2;
+
+export function normRad(angle) {
+  let mod = angle % angle360;
+  if (mod < -angle180) {
+    mod += angle360;
+  } else if (mod > angle180) {
+    mod -= angle360;
+  }
+  return mod;
 }
