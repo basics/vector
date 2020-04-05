aa8cf024f099efa189cf10e8d312552bc161d744
Stefan Eckert <stefan.eckert@sportalliance.com>
2020-04-05 10:08:46 +0200

    feat(style): split to Style and TemplateStyle

--- src/style.js
+++ src/style.js
@@ -1,42 +1,46 @@
-const NAME = Symbol('name');
-const TEMPLATE = Symbol('template');
-const TARGET = Symbol('target');
-const KEYS = Symbol('keys');
-
-function getCSSVars(name, target, keys) {
-  let prefix = '';
-  if (name) {
-    prefix = `-${name}-`;
+export class Style {
+  constructor(name = undefined) {
+    this.name = name;
   }
 
-  return keys.reduce((res, key) => {
-    const t = target[key];
-    if (t.toCSSVars) {
-      t.toCSSVars(`${prefix}${key}`, res);
-    } else {
-      res[`-${prefix}-${key}`] = t.toString();
+  get cssVars() {
+    let prefix = '';
+    const { name } = this;
+    if (name) {
+      prefix = `${name}-`;
     }
-    return res;
-  }, {});
+
+    return Object.entries(this).reduce((res, [key, t]) => {
+      if (key === 'name' || key === 'cssVars' || key === 'template') {
+        return res;
+      }
+      if (t.toCSSVars) {
+        t.toCSSVars(`${prefix}${key}`, res);
+      } else {
+        res[`--${prefix}${key}`] = t.toString();
+      }
+      return res;
+    }, {});
+  }
 }
 
-const handler = {
-  get(obj, prop) {
-    if (prop === 'cssTemplate') {
-      return obj[TEMPLATE];
-    }
-    if (prop === 'cssVars') {
-      return getCSSVars(obj[NAME], obj, obj[KEYS]);
-    }
-    return obj[prop];
+export class TemplateStyle extends Style {
+  constructor(template, name = undefined) {
+    super(name);
+    this.template = template;
   }
-};
 
-export function style({ name, template, ...definition }) {
-  const proxy = new Proxy(definition, handler);
-  proxy[NAME] = name;
-  proxy[TEMPLATE] = template;
-  proxy[TARGET] = definition;
-  proxy[KEYS] = Object.keys(definition);
-  return proxy;
+  bakeStyle() {
+    const { cssVars } = this;
+    const vars = Object.entries(cssVars).map(([key, v]) => `${key}: ${v}`).join((';\n'));
+    return `\n${this.template}\n${vars}\n`;
+  }
+}
+
+export function style(name = undefined) {
+  return new Style(name);
+}
+
+export function templateStyle(template, name = undefined) {
+  return new TemplateStyle(template, name);
 }
