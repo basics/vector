6cd5be387327c05cf410b6bf3d3ece637d9e2d28
Stefan Eckert <stefan.eckert@sportalliance.com>
2021-05-13 13:41:50 +0200

    fix(operator): fix handling with number wrappers

--- test/operator.js
+++ test/operator.js
@@ -3,16 +3,41 @@ import { operatorCalc, cachedFactory } from '../src/operator';
 import { victor, vector } from '../src';
 
 describe('operatorCalc with Victor test.', () => {
-  it('should throw error with assgined Victor', () => {
+  it('should throw error with assingned Victor', () => {
     const pos = victor(5, 6, 7);
     const dir = victor(1, 0, 0);
 
     assert.throws(() => operatorCalc(() => dir * pos, victor(1, 0, 0)));
   });
+
+  it('should work fine with number wrapper class', () => {
+    class Num {
+      constructor(nr) {
+        this.nr = nr;
+      }
+
+      valueOf() {
+        return this.nr;
+      }
+
+      toString() {
+        return `${this.nr}`;
+      }
+    }
+
+    const pos = vector(new Num(5), 6, 7);
+    const dir = vector(1, new Num(0), 0);
+
+    const scale = operatorCalc(() => dir * pos, vector(1, 0.9, 0));
+
+    assert.equal(scale.x, 5);
+    assert.equal(scale.y, 0);
+    assert.equal(scale.z, 0);
+  });
 });
 
 describe('operatorCalc with Vector test.', () => {
-  it('should throw error with assgined Victor', () => {
+  it('should work fine with assigning result vector', () => {
     const pos = vector(5, 6, 7);
     const dir = vector(1, 0, 0);
 
