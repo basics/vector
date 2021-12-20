a7554a8716b8d55ecfe716a9a5fdeb7e0a1e6001
Stephan Gerbeth <stephan.gerbeth@grabarzundpartner.de>
2021-12-20 13:34:01 +0100

    fix(esbuild): different builds cjs/ems/iife

--- build.js
+++ build.js
@@ -1,11 +1,46 @@
 import { build } from 'esbuild';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
 
-build({
-  entryPoints: ['./src/index.js'],
+const inputs = [
+  { src: './src/index.js', name: 'basics.vector', target: '' },
+  { src: './src/operator.js', name: 'basics.vector.operator' },
+  { src: './src/adapter/playcanvas.js', name: 'basics.vector.adapter.playcanvas'}
+];
+
+const defaultOptions = {
   bundle: true,
   minify: true,
   sourcemap: true,
+  treeShaking: true,
   target: browserslistToEsbuild(),
-  outfile: './build/out.js',
-}).catch(() => global.process.exit(1));
+  mainFields: ['module', 'main'],
+  outbase: 'src'
+};
+
+inputs.forEach((item) => {
+  build({
+    ...defaultOptions,
+    entryPoints: [item.src],
+    format: 'iife',
+    globalName: item.name,
+    outdir: './build/iife',
+  }).catch(() => global.process.exit(1));
+
+  build({
+    ...defaultOptions,
+    entryPoints: [item.src],
+    format: 'esm',
+    splitting: true,
+    outdir: './build/esm/',
+    outExtension: { '.js': '.mjs' }
+  }).catch(() => global.process.exit(1));
+
+  build({
+    ...defaultOptions,
+    entryPoints: [item.src],
+    format: 'cjs',
+    outdir: './build/cjs/',
+    outExtension: { '.js': '.cjs' }
+  }).catch(() => global.process.exit(1));
+});
+
