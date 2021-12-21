7df6c072c4c4e7871577d2444d827e16483f7e37
Stefan Eckert <stefan.eckert@sportalliance.com>
2021-12-21 18:18:45 +0100

    fix(formatter): number format fix typo

--- test/formatter.js
+++ test/formatter.js
@@ -3,7 +3,7 @@ import { formatNumber } from '../src/formatter';
 
 describe('formatNumber test.', () => {
   it('should format Number with default 4 decimals', () => {
-    assert.equal(formatNumber(0.00000001), '0.000000001');
+    assert.equal(formatNumber(0.00000001), '0.00000001');
     assert.equal(formatNumber(0.00009998), '0.00009998');
     assert.equal(formatNumber(0.00000000000000000001), '0.00000000000000000001');
     assert.equal(formatNumber(0.0000000000000000000000000001), '0.0000000000000000000000000001');
